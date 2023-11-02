f494b56ab862ceca64976d590cd7d33960efd48a
Are Schjetne <sixcare.as@gmail.com>
2023-11-02 08:42:51 +0100

    Install mullvad, download configs and correct package name

--- install.sh
+++ install.sh
@@ -3,20 +3,37 @@
 set -euo pipefail
 
 doas apt-get update
-doas apt-get install -y ca-certificates curl gnupg build-essentials htop bmon net-tools python3-venv
+doas apt-get install -y ca-certificates curl gnupg build-essential htop bmon net-tools python3-venv
 doas install -m 0755 -d /etc/apt/keyrings
+
+mkdir -p ~/Downloads/
+
+# Mullvad
+curl -fsSLo ~/Downloads/mullvad.deb https://mullvad.net/download/app/deb/latest
+doas apt-get install -y ~/Downloads/mullvad.deb
+rm -f ~/Downloads/mullvad.deb
 
 # Git
 git config --global init.defaultBranch main
 git config --global user.name "Are Schjetne"
 git config --global user.email sixcare.as@gmail.com
 
+# Nerdfonts
+mkdir -p ~/Downloads/fonts
+curl -fsSLo ~/Downloads/SourceCodePro.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/SourceCodePro.zip
+unzip ~/Downloads/SourceCodePro.zip -d ~/Downloads/fonts
+doas mv ~/Downloads/fonts/*.ttf /usr/local/share/fonts/
+rm -rf ~/Downloads/fonts ~/Downloads/SourceCodePro.zip
+
 # Sway
 doas apt-get install -y sway swaybg swayidle swaylock waybar
-mkdir -p ~/.config/{sway,waybar,foot}
-cp config/swayconfig ~/.config/sway/config
-cp -R config/waybar ~/.config/waybar/
-cp config/foot.ini ~/.config/foot/foot.ini
+mkdir -p ~/.config/{sway,foot}
+curl -fsSLo ~/.config/sway/config https://raw.githubusercontent.com/sixcare/dotfiles/main/config/swayconfig
+curl -fsSLo ~/.config/foot/foot.ini https://raw.githubusercontent.com/sixcare/dotfiles/main/config/foot.ini
+mkdir -p ~/.config/waybar/scripts
+curl -fsSLo ~/.config/waybar/styles.css https://raw.githubusercontent.com/sixcare/dotfiles/main/config/waybar/style.css
+curl -fsSLo ~/.config/waybar/config https://raw.githubusercontent.com/sixcare/dotfiles/main/config/waybar/config
+curl -fsSLo ~/.config/waybar/mullvad.zsh https://github.com/sixcare/dotfiles/blob/main/config/waybar/scripts/mullvad.zsh
 
 # Podman
 doas apt-get install -y podman
@@ -47,23 +64,15 @@ doas apt install -y signal-desktop
 # VIM
 doas apt-get install vim
 doas update-alternatives --set editor /usr/bin/vim.basic
-cp config/vimrc ~/.vimrc
+curl -fsSLo ~/.vimrc https://raw.githubusercontent.com/sixcare/dotfiles/main/config/vimrc
 
 # TMUX
 doas apt-get install -y tmux
-cp config/tmux.conf ~/.tmux.conf
-
+curl -fsSLo ~/.tmux.conf https://raw.githubusercontent.com/sixcare/dotfiles/main/config/tmux.conf
 
 # Neovim
 doas curl -fsSLo /usr/local/bin/nvim https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 doas chmod +x /usr/local/bin/nvim
-
-# Nerdfonts
-mkdir -p ~/Downloads/fonts
-curl -fsSLo ~/Downloads/SourceCodePro.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/SourceCodePro.zip
-unzip ~/Downloads/SourceCodePro.zip -d ~/Downloads/fonts
-doas mv ~/Downloads/fonts/*.ttf /usr/local/share/fonts/
-rm -rf ~/Downloads/fonts ~/Downloads/SourceCodePro.zip
 
 # nvim
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
@@ -76,10 +85,8 @@ doas apt-get install spotify-client
 
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
 nvm use node
 
 # ShellCheck
