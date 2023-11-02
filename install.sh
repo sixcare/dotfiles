fd981a1c251fb705a6f4899c4314345871298104
Are Schjetne <sixcare.as@gmail.com>
2023-11-02 10:04:20 +0100

    Install firefox

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ log() {
 
 log "Packages"
 doas apt-get update
-doas apt-get install -y ca-certificates curl gnupg unzip build-essential htop bmon net-tools python3-venv
+doas apt-get install -y ca-certificates curl firefox-esr firefox-esr-l10n-en-gb firefox-esr-l10n-nb-no gnupg unzip build-essential htop bmon net-tools python3-venv
 
 mkdir -p ~/Downloads/
 
