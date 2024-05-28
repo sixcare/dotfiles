2895fb3abab33325e4fb7118d4c1cb8a99182e46
Are Schjetne <sixcare.as@gmail.com>
2024-05-28 21:02:41 +0200

    🚨 Fix lint errors

--- scripts/nc_mnt.sh
+++ scripts/nc_mnt.sh
@@ -20,24 +20,24 @@ else
 fi
 
 log "Check if $BACKUP_PATH exists"
-if [ ! -d $BACKUP_PATH ]; then
+if [ ! -d "$BACKUP_PATH" ]; then
     log "$BACKUP_PATH does not exists. Creating it."
-    mkdir -p $BACKUP_PATH
+    mkdir -p "$BACKUP_PATH"
 else
     log "It does exist. Proceeding"
 fi
 
 log "Checking number of backups"
-if [ $(find $BACKUP_PATH -name "*-passwords.kdbx.xz" -type f | wc -l) -gt $BACKUP_NUMBERS ]; then
+if [ "$(find "$BACKUP_PATH" -name "*-passwords.kdbx.xz" -type f | wc -l)" -gt $BACKUP_NUMBERS ]; then
     log "There are more than $BACKUP_NUMBERS backup files. Removing excess."
-    find $BACKUP_PATH -name "*-passwords.kdbx.xz" -type f -printf "%T@ %p\n" | sort -n | head -n -$BACKUP_NUMBERS | cut -d' ' -f2- | xargs -I {} rm -- "{}"
+    find "$BACKUP_PATH" -name "*-passwords.kdbx.xz" -type f -printf "%T@ %p\n" | sort -n | head -n -$BACKUP_NUMBERS | cut -d' ' -f2- | xargs -I {} rm -- "{}"
 else
     log "Number of backups are less than $BACKUP_NUMBERS. We're good to go"
 fi
 
 log "Taking a backup"
 TIMESTAMP=$(date +%Y%m%dT%H%M)
-xz --compress --keep --check=crc64 --stdout $BACKUP_FILE > $BACKUP_PATH/$TIMESTAMP-passwords.kdbx.xz
+xz --compress --keep --check=crc64 --stdout "$BACKUP_FILE" > "$BACKUP_PATH/$TIMESTAMP-passwords.kdbx.xz"
 
 log "Backup complete. $BACKUP_PATH/$TIMESTAMP-passwords.kdbx.xz created"
 
