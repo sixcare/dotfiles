de070e44fa4fb0e5c3169b805a2e0534d2633b97
Are Schjetne <sixcare.as@gmail.com>
2025-06-01 09:10:41 +0100

    ✨ Re-write wireguard VPN check

--- config/waybar/scripts/wireguard.sh
+++ config/waybar/scripts/wireguard.sh
@@ -2,35 +2,21 @@
 
 set -o pipefail
 
-WG_SERVERS=(
-  no-osl-wg-001
-  no-osl-wg-002
-  no-osl-wg-003
-  no-osl-wg-004
-  no-osl-wg-005
-  no-osl-wg-006
-)
-INTERFACE=NULL
+readarray -t INTERFACES < <(ip -details -j l show type wireguard | jq -r '.[]["ifname"]')
 
-for i in "${!WG_SERVERS[@]}"; do
-  if ip link show dev "${WG_SERVERS[$i]}" &>/dev/null; then
-    INTERFACE=${WG_SERVERS[$i]}
-    break
-  fi
-done
-
-if [[ ${INTERFACE} == NULL ]]; then
+if [ ${#INTERFACES[@]} -eq 0 ]; then
   printf "%s\n" "{\"text\":\"󰅛 X\",\"tooltip\":\"No WireGuard interface detected\"}"
   exit 0
 fi
 
-ENDPOINT=$(doas wg show | grep 'endpoint:')
-ENDPOINT_EXEC_STATUS=$?
-if [[ ${ENDPOINT_EXEC_STATUS} != 0 ]]; then
-  printf "%s\n" "{\"text\":\"󰅛 X\",\"tooltip\":\"Failed to get WireGuard status\"}"
-  exit 0
-fi
-ENDPOINT=${ENDPOINT##*: }
+TOOLTIP_TEXT=$"WireGuard connected to ${#INTERFACES[@]} interface(s)"
+for INTERFACE in "${INTERFACES[@]}"; do
+    ENDPOINT=0
+    ENDPOINT=$(grep Endpoint /etc/wireguard/"${INTERFACE}".conf)
+    if [[ "${ENDPOINT}" != 0 ]]; then
+      TOOLTIP_TEXT+=$" ${INTERFACE}(${ENDPOINT##*= })"
+    fi
+done
 
-printf "%s\n" "{\"text\":\"󰒄 \",\"tooltip\":\"WireGuard connected to ${INTERFACE} (${ENDPOINT})\"}"
+printf "%s\n" "{\"text\":\"󰒄 \",\"tooltip\":\"${TOOLTIP_TEXT}\"}"
 exit 0
