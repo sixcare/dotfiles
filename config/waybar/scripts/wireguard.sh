cbcd0a0ac58cb7b008c710a8b9496f738420183f
Are Schjetne <sixcare.as@gmail.com>
2025-06-02 09:19:13 +0100

    🐛 Fix null value bug

--- config/waybar/scripts/wireguard.sh
+++ config/waybar/scripts/wireguard.sh
@@ -2,21 +2,34 @@
 
 set -o pipefail
 
-readarray -t INTERFACES < <(ip -details -j l show type wireguard | jq -r '.[]["ifname"]')
+readarray -t INTERFACES < <(ip -details -j l show type wireguard | jq -r '.[] | select ( . != {}) ["ifname"]')
 
 if [ ${#INTERFACES[@]} -eq 0 ]; then
   printf "%s\n" "{\"text\":\"󰅛 X\",\"tooltip\":\"No WireGuard interface detected\"}"
   exit 0
 fi
 
-TOOLTIP_TEXT=$"WireGuard connected to ${#INTERFACES[@]} interface(s)"
+i=0
+IFENDPOINTS=()
 for INTERFACE in "${INTERFACES[@]}"; do
     ENDPOINT=0
-    ENDPOINT=$(grep Endpoint /etc/wireguard/"${INTERFACE}".conf)
+    ENDPOINT=$(grep -s Endpoint /etc/wireguard/"${INTERFACE}".conf)
+    if [[ "${ENDPOINT}" == "" ]]; then
+      continue
+    fi
+
     if [[ "${ENDPOINT}" != 0 ]]; then
-      TOOLTIP_TEXT+=$" ${INTERFACE}(${ENDPOINT##*= })"
+      (( i+=1 ))
+      IFENDPOINTS+=($" ${INTERFACE}(${ENDPOINT##*= })")
     fi
 done
+
+TOOLTIP_TEXT=$(printf "Connected to ${i} WireGuard interface(s)%s" "${IFENDPOINTS[@]}")
+
+if [[ "${i}" == 0 ]]; then
+  printf "%s\n" "{\"text\":\"󰅛 X\",\"tooltip\":\"Failed to find wireguard interface\"}"
+  exit 0
+fi
 
 printf "%s\n" "{\"text\":\"󰒄 \",\"tooltip\":\"${TOOLTIP_TEXT}\"}"
 exit 0
