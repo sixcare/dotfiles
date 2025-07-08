782a3fa811398958edacfa6a11f8eee23e3b107a
Are Schjetne <sixcare.as@gmail.com>
2025-07-08 07:01:41 +0100

    🔧 Add Neovim leader and shortcuts

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -8,4 +8,8 @@ vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
 
+vim.g.mapleader = ","
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>t', '<C-w>h', { desc = 'Focus Neo-tree' })
+
 require("config.lazy")
