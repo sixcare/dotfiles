b134483469e7fb4838a346939c868754510690b0
Are Schjetne <sixcare.as@gmail.com>
2025-11-08 11:17:48 +0100

    ✨ Add function for starting up tmux

---
+++ scripts/tmux_start.sh
@@ -0,0 +1,23 @@
+tmux_start () {
+    session="daily"
+
+    if tmux has-session -t $session 2>/dev/null; then
+        printf "%s\n" "Session is started"
+        exit 1
+    fi
+
+	tmux start-server
+	tmux new-session -d -s $session
+
+	tmux rename-window -t $session "[btop]"
+	tmux selectp -t 0
+	tmux send-keys "btop" C-m
+
+	tmux new-window -t $session -n "[journalctl]"
+	tmux selectp -t 1
+	tmux send-keys "doas journalctl" C-m
+
+    tmux attach-session -t $session
+}
+
+tmux_start
