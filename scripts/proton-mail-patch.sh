dcfc56ce7e0d6a43917709792d34c59558855740
Are Schjetne <sixcare.as@gmail.com>
2025-11-09 07:09:13 +0100

    🤡 Replace duplicated proton pass patch script with the actual proton mail patch script

--- scripts/proton-mail-patch.sh
+++ scripts/proton-mail-patch.sh
@@ -2,13 +2,14 @@
 
 set -euo pipefail
 
-VERSION_URL="https://proton.me/download/PassDesktop/linux/x64/version.json"
+VERSION_URL="https://proton.me/download/mail/linux/version.json"
 VERSION_JSON=$(curl -fsL "${VERSION_URL}")
 
 
-VERSION=$(echo "${VERSION_JSON}" | jq -r '.Releases[0].Version')
-SHA=$(echo "${VERSION_JSON}" | jq -r '.Releases[0].File[] | select(.Identifier==".deb (Ubuntu/Debian)") | .Sha512CheckSum')
-DOWNLOAD_URL=$(echo "${VERSION_JSON}" | jq -r '.Releases[0].File[] | select(.Identifier==".deb (Ubuntu/Debian)") | .Url')
+VERSION=$(echo "${VERSION_JSON}" | jq -r '[.Releases[] | select(.CategoryName=="Stable")][0].Version')
+SHA=$(echo "${VERSION_JSON}" | jq -r '[.Releases[] | select(.CategoryName=="Stable")][0].File[] | select(.Identifier==".deb (Ubuntu/Debian)").Sha512CheckSum')
+DOWNLOAD_URL=$(echo "${VERSION_JSON}" | jq -r '[.Releases[] | select(.CategoryName=="Stable")][0].File[] | select(.Identifier==".deb (Ubuntu/Debian)").Url')
+
 
 if [ ! "$VERSION" ]; then
     printf "%s\n" "❌ Failed to get version" 1>&2 
@@ -23,16 +24,16 @@ if [ ! "$DOWNLOAD_URL" ]; then
 fi
 
 printf "%s\n" "⬇️ Downloading version ${VERSION}"
-curl -fsLo "/tmp/proton-pass-${VERSION}.deb" "${DOWNLOAD_URL}"
+curl -fsLo "/tmp/proton-mail-${VERSION}.deb" "${DOWNLOAD_URL}"
 
 printf "%s\n" "🔐 Checking SHA"
-echo "${SHA} /tmp/proton-pass-${VERSION}.deb" | sha512sum --check -
+echo "${SHA} /tmp/proton-mail-${VERSION}.deb" | sha512sum --check -
 
 printf "%s\n" "📦 Installing"
-doas dpkg -i "/tmp/proton-pass-${VERSION}.deb"
+doas dpkg -i "/tmp/proton-mail-${VERSION}.deb"
 
 printf "%s\n" "📦 Cleaning up"
-rm "/tmp/proton-pass-${VERSION}.deb"
+rm "/tmp/proton-mail-${VERSION}.deb"
 
 printf "%s\n" "✅ Done!"
 
